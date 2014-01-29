f5610705ccfa0cc127877067f095873a9e503e4e
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-01-30 01:32:55 +0530

    Java Palindrome checker - made it more generic to check for number or text

--- java/Palindrome.java
+++ java/Palindrome.java
@@ -24,15 +24,15 @@
 public class Palindrome {
 	public static void main(String[] args) {
 		if(args.length == 0 || args.length > 1) {
-			System.out.println("Usage: java PalindromeTest <number>");
+			System.out.println("Usage: java Palindrome <text>");
 			System.exit(-1);
 		}
 		
 		String revString = new StringBuffer(args[0]).reverse().toString();
 		
 		if(revString.equals(args[0]))
-			System.out.println(args[0] + " is palindrome number.");
+			System.out.println(args[0] + " is palindrome.");
 		else
-			System.out.println(args[0] + " is not palindrome number.");
+			System.out.println(args[0] + " is not palindrome.");
 	}
 }
