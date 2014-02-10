9fd6e2327dae78a552492ba1a0dc06beeb0bfd37
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-02-10 14:28:23 +0530

    Ruby: A script to print your full name

---
+++ ruby/puts_full_name.rb
@@ -0,0 +1,29 @@
+#  puts_full_name.rb
+#  
+#  Copyright 2014 Ajay Bhatia <ajay@dumb-box>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+puts "Hi! What\'s your first name?"
+first_name = gets.chomp
+puts "Middle name?"
+middle_name = gets.chomp
+puts "And last name?"
+last_name = gets.chomp
+
+puts "Pleased to meet you, #{first_name} #{middle_name} #{last_name}"
