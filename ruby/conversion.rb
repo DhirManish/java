44af9f9adf0c109dd177d9f2db9ea528d9969034
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-02-10 14:18:21 +0530

    Ruby: A script to convert string to integer and integer to string

---
+++ ruby/conversion.rb
@@ -0,0 +1,27 @@
+#  conversion.rb
+#  
+#  Copyright 2014 Ajay Bhatia <ajay@dumb-box>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+var1 = 2
+var2 = '5'
+
+puts var1.to_s + var2
+
+puts var1 + var2.to_i
