c7fa2fb6f03190d1006cadcc4f7f00bad4c47108
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-05-21 16:22:33 +0530

    java: Added some basic programmes (Introduction Part)

---
+++ java/Greeting.java
@@ -0,0 +1,11 @@
+public class Greeting {
+	public static void main(String[] args) {
+		System.out.print("Hello, ");
+		for (int i = 0; i < args.length; i++)
+			System.out.print(args[i] + " ");
+
+		System.out.println();
+	}
+}
+
+
---
+++ java/MultiplyTab.java
@@ -0,0 +1,13 @@
+public class MultiplyTab {
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(i + " X " + j + " = " + (i * j));			
+			}		
+			System.out.println();
+		}
+		
+	}
+}
