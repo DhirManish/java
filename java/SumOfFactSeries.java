907d2c3c47772173357b6ee2cb86720520985ad9
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-05-23 10:53:09 +0530

    Java: Sum of Factorial series amd it's output.

---
+++ java/SumOfFactSeries.java
@@ -0,0 +1,25 @@
+// 1! + 2! + 3! + .. + n! = SUM
+
+import java.util.Scanner;
+
+public class SumOfFactSeries {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the limit: ");
+		int n = scan.nextInt();
+		
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			int fact = 1;
+			for (int j = 1; j <= i; j++) 
+				fact *= j;
+			
+			sum += fact;
+			
+			System.out.print((i != n) ? (i + "! + ") : (i + "!")); 
+		}
+		
+		System.out.println(" = " + sum);
+		
+	}
+}
