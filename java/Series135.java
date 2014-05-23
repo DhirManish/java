eb0056561f51fbad5d5a3a0acdb768ed0b726c73
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-05-23 10:32:39 +0530

    Java: Sum of odd series and its output on default output device

---
+++ java/Series135.java
@@ -0,0 +1,22 @@
+// 1 + 3 + 5 ... + nth = SUM
+
+public class Series135 {
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		
+		int i = 1, s = 1, sum = 0;
+		while (i <= n) {
+			if (i != n)
+				System.out.print(s + " + ");
+			else
+				System.out.print(s);
+				
+			sum += s;
+			
+			s += 2;
+			i++;
+		}
+		
+		System.out.println(" = " + sum);
+	}
+}
