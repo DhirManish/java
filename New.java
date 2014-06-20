aef4ac970f0665167a9db88ab75b0d0d65433f1b
Manish Dhir <manishdhir4@gmail.com>
2014-06-20 15:38:22 +0530

    little modications

--- New.java
+++ New.java
@@ -1,7 +1,6 @@
 public class New {
      public static void main(String[] args){
 	int a=Integer.parseInt(args[0]);
-	//javax.swing.JOptionPane.showMessageDialog(null, a);
 	int squre=a*a;
 	javax.swing.JOptionPane.showMessageDialog(null,"squre of "+a+" is " +squre);
 	}
