cab5d04c96a6f62b33c3bb3dc15e0a600f68a56f
Manish Dhir <manishdhir4@gmail.com>
2014-06-25 00:09:15 +0530

    a simple program to enter the login id and password only

--- Login.java
+++ Login.java
@@ -4,9 +4,9 @@ public class Login {
      public static void main(String[] args) {
 	String username = JOptionPane.showInputDialog(null, "Enter you name?");
 	int password=Integer.parseInt(JOptionPane.showInputDialog(null,"enter the password"));
-	      if(username=="Abc" && password==123)
+	      if(username.equals("Abc") && password==123)
 		JOptionPane.showMessageDialog(null, "Welcome, " + username, "Welcome Message", JOptionPane.PLAIN_MESSAGE);
-	      else if(username=="ram" && password==000)
+	      else if(username.equals("ram") && password==000)
 	        JOptionPane.showMessageDialog(null, "Welcome, " + username, "Welcome Message", JOptionPane.PLAIN_MESSAGE);
 	      else                                               
 JOptionPane.showMessageDialog(null, "your username or password is wrong" , "error massage", JOptionPane.PLAIN_MESSAGE);
