d995a353b8d4157d7d80eaf052cf83fa68882899
Manish Dhir <manishdhir4@gmail.com>
2014-06-25 00:24:45 +0530

    a simple program to enter the login id and password only

--- Login.java
+++ Login.java
@@ -4,21 +4,18 @@ public class Login {
      public static void main(String[] args) {
 	String username = JOptionPane.showInputDialog(null, "Enter you name?");
 	int password=Integer.parseInt(JOptionPane.showInputDialog(null,"enter the password"));
-<<<<<<< HEAD
 	      if(username.equals("Abc") && password==123)
 		JOptionPane.showMessageDialog(null, "Welcome, " + username, "Welcome Message", JOptionPane.PLAIN_MESSAGE);
 	      else if(username.equals("ram") && password==000)
-=======
-	      if(username=="Abc" && password==123)
+		 if(username=="Abc" && password==123)
 		JOptionPane.showMessageDialog(null, "Welcome, " + username, "Welcome Message", JOptionPane.PLAIN_MESSAGE);
 	      else if(username=="ram" && password==000)
->>>>>>> 84d6718eef5b164a6b91859716038be907195cd4
 	        JOptionPane.showMessageDialog(null, "Welcome, " + username, "Welcome Message", JOptionPane.PLAIN_MESSAGE);
 	      else                                               
-JOptionPane.showMessageDialog(null, "your username or password is wrong" , "error massage", JOptionPane.PLAIN_MESSAGE);
+          JOptionPane.showMessageDialog(null, "your username or password is wrong" , "error massage", JOptionPane.PLAIN_MESSAGE);
 	}
 }
 
 
 
-//this program is not working for if and else if condition it gives the last statement your username or password is wrong 
+
